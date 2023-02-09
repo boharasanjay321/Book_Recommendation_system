25fc66585d6bf6c49076d8e10ac9fa8132b40afa
sanjay <bohara.sanjay@gmail.com>
2023-02-09 16:54:47 +0530

    update

---
+++ Book_System/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPEN_E_DOT="-e"
+def get_requirements()->[str]:
+
+    with open(REQUIREMENT_FILE_NAME) as requirements_file:
+       requirement_list= requirements_file.readlines()
+       requirement_list=[requirement_name.repalce("\n","") for requirement_name in requirement_list]
+    if HYPEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPEN_E_DOT)
+    return requirement_list
