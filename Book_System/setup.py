085f5aeeee04b39d8c41f7f7eb98aeecb37a5ede
sanjay <bohara.sanjay@gmail.com>
2023-02-10 15:21:32 +0530

    new

--- Book_System/setup.py
+++ Book_System/setup.py
@@ -8,4 +8,14 @@ def get_requirements()->[str]:
        requirement_list=[requirement_name.repalce("\n","") for requirement_name in requirement_list]
     if HYPEN_E_DOT in requirement_list:
         requirement_list.remove(HYPEN_E_DOT)
+        
     return requirement_list
+
+    setup(
+     name="brs",  
+     version="0.0.2",
+     author='sanjay',
+     package=find_packages(),
+     install_requires=get_requirements(),
+
+    )
